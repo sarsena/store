6760f7b6c8c419761f8cdabfb7005cf1f9f529fb
Steven Arsena <sjarsena@gmail.com>
2013-11-13 20:59:46 -0500

    removing migrations

--- db/migrate/20131031014309_create_order_products.rb
+++
@@ -1,8 +0,0 @@
-class CreateOrderProducts < ActiveRecord::Migration
-  def change
-    create_table :order_products do |t|
-
-      t.timestamps
-    end
-  end
-end
--- test/unit/customer_test.rb
+++ test/unit/customer_test.rb
@@ -4,4 +4,8 @@ class CustomerTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "age" do
+    age =  Customer.age("12-19-1988")
+    assert age = 24
+  end
 end
